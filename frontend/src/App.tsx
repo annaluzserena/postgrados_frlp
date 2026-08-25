de9b1fd2235ab8e9e74b718dfb03caf0a6888d82
nardulli03 <ornenardulli@gmail.com>
2026-08-25 12:46:51 -0300

    Fix: Agrego Modulo de Legajo actualizado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <LoginScreen />
   );
-}*/
+}
 
 
 import NotificacionesScreen from './AlertasNotificaciones/pages/Alerta';
@@ -23,6 +23,15 @@ import NotificacionesScreen from './AlertasNotificaciones/pages/Alerta';
 function App() {
   return (
     <NotificacionesScreen />
+  );
+}
+export default App;*/
+
+import  Dashboard from './dashboard-coordinador/pages/Dashboard';
+
+function App() {  
+   return (
+    <Dashboard />
   );
 }
 export default App;
