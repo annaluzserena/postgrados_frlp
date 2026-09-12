c09cbae182aab9a9ed3185b42bb544635eb2215b
St3v3 M4qu1n4 <annaluzserena@protonmail.com>
2026-09-12 00:07:44 -0300

    feat: agrego rutas para panel, inscriptos, detalle legajo y consultar estado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from './dashboard-coordinador/pages/Dashboard';
 import LoginScreen from "./Login/pages/Login";
 import WelcomeScreen from "./Login/pages/WelcomeScreen";
+import { ConsultarEstado } from "./inscripcion-aspirante/pages/ConsultarEstado";
 import NotFound from "./shared/pages/NotFound";
 import InscripcionWizard from './inscripcion-aspirante/pages/InscripcionWizard';
 
@@ -11,8 +12,11 @@ export default function App() {
       <Routes>
         <Route path="/">
           <Route index element={<LoginScreen />}/>
-          <Route path="dashboard" element={<Dashboard />}/>
+          <Route path="panel" element={<Dashboard path="/panel"/>}/>
+          <Route path="inscriptos" element={<Dashboard path="/inscriptos" />} />
+          <Route path="inscriptos/:id" element={<Dashboard path="/:id" />} />
           <Route path="welcome" element={<WelcomeScreen />}/>
+          <Route path="consultar" element={<ConsultarEstado />} />
           <Route path="inscripcion" element={<InscripcionWizard />}/>
           <Route path="*" element={<NotFound />}/>
         </Route>
