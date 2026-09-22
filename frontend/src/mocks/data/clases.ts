b4b1416ba7317d8136cf6f003b3b8701ac2a5e57
SolangeRodrigo <solangeabigailrodrigo@gmail.com>
2026-09-22 00:20:02 -0300

    feat: Agregue en mocks>data>clases.ts para poder runnear y testear la informacion

---
+++ frontend/src/mocks/data/clases.ts
@@ -0,0 +1,9 @@
+import type { Alumno, Clase } from "@/shared/types/types";
+
+export const alumnosFixture: Alumno[] = [
+  { id: "al-001", apellido: "Gómez", nombre: "Lucía" },
+  { id: "al-002", apellido: "Fernández", nombre: "Tomás" },
+  { id: "al-003", apellido: "Ibáñez", nombre: "Carla" },
+];
+
+export const clasesFixture: Clase[] = [];
